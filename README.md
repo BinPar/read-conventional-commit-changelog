0e1d2f84c929e11f2710344e4fa57fe894419615
Marcos González Andrés <marcos@binpar.com>
2021-07-08 10:52:37 +0200

    First version

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# read-conventional-commit-changelog
+<!-- start title -->
+
+# GitHub Action: Read conventional commit CHANGELOG
+
+<!-- end title -->
+<!-- start description -->
+
+Reads the conventional commit generated CHANGELOG and outputs the requested version changes
+
+<!-- end description -->
+<!-- start contents -->
+<!-- end contents -->
+<!-- start usage -->
+
+```yaml
+- uses: BinPar/read-conventional-commit-changelog@v1.0.0
+  with:
+    # The CHANGELOG path. Defaults to ./CHANGELOG.md
+    # Default: ./CHANGELOG.md
+    changelog-path: ""
+
+    # The version of the changelog to output. Defaults to latest
+    # Default: latest
+    version: ""
+```
+
+<!-- end usage -->
+<!-- start inputs -->
+
+| **Input**            | **Description**                                            |   **Default**    | **Required** |
+| :------------------- | :--------------------------------------------------------- | :--------------: | :----------: |
+| **`changelog-path`** | The CHANGELOG path. Defaults to ./CHANGELOG.md             | `./CHANGELOG.md` |  **false**   |
+| **`version`**        | The version of the changelog to output. Defaults to latest |     `latest`     |  **false**   |
+
+<!-- end inputs -->
+<!-- start outputs -->
+
+| **Output**          | **Description**               | **Default** | **Required** |
+| :------------------ | :---------------------------- | ----------- | ------------ |
+| `version-changelog` | The requested version changes |             |              |
+
+<!-- end outputs -->
+<!-- start [.github/ghdocs/examples/] -->
+<!-- end [.github/ghdocs/examples/] -->
---
+++ index.js
@@ -0,0 +1,41 @@
+const core = require('@actions/core');
+const fs = require('fs/promises');
+
+async function main() {
+  try {
+    const changelogPath = core.getInput('changelog-path') || './CHANGELOG.md';
+    core.debug(`CHANGELOG Path: ${changelogPath}`);
+    const versionToRetrieve = core.getInput('version') || 'latest';
+    core.debug(`Version: ${versionToRetrieve}`);
+    let simpleVersion = versionToRetrieve.replace(/^v/, '');
+    const changelogStr = await fs.readFile(changelogPath, { encoding: 'utf-8' });
+    if (simpleVersion === 'latest') {
+      const versionHeaderRegExp = /### ?\[?(\d+.\d+.\d+)\]?/;
+      const headerMatch = changelogStr.match(versionHeaderRegExp);
+      if (headerMatch && headerMatch[1]) {
+        [, simpleVersion] = headerMatch;
+      }
+    }
+    let headerVersionIndex = changelogStr.indexOf(`### [${simpleVersion}]`);
+    if (headerVersionIndex === -1) {
+      headerVersionIndex = changelogStr.indexOf(`### ${simpleVersion}`);
+      if (headerVersionIndex === -1) {
+        throw new Error(
+          `The version "${simpleVersion}" can't be retrieved from the CHANGELOG located in ${changelogPath}`,
+        );
+      }
+    }
+    const changelogFromVersion = changelogStr.substr(headerVersionIndex);
+    const [, nextVersionHeader] = changelogFromVersion.match(/### ?\[?(\d+.\d+.\d+)\]?/g);
+    core.setOutput(
+      'version-changelog',
+      nextVersionHeader
+        ? changelogFromVersion.substring(0, changelogFromVersion.indexOf(nextVersionHeader))
+        : changelogFromVersion,
+    );
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+main().catch((error) => core.setFailed(error.message));
